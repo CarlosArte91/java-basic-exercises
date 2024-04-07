c40afb7324b50e3968ac40e2739d1583a9ec4aec
carlos cruz <carturo99@gmail.com>
2024-04-07 17:40:25 -0500

    Task: JB-001 Message: Basic Variables Exercises
    The most basic exercise of variables is added.

---
+++ src/variables/BasicVariables.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class BasicVariables {
+    public static void main(String[] args) {
+        /*
+        * As a good rule of thumb, in Java, always make
+        * sure to initialize variables when you create them.
+        * */
+        int speed = 80;
+        String employeeName = "Carlos Cruz";
+
+        System.out.println("The speed is " + speed);
+        System.out.println("Employee name is " + employeeName);
+    }
+}
