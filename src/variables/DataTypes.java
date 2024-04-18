98323cafefe2111268be128040d639a388c7e4c0
carlos cruz <carturo99@gmail.com>
2024-04-18 17:28:28 -0500

    Task: JB-003 Message: Numeric variables
    Numeric data types with their minimum and maximum values.

---
+++ src/variables/DataTypes.java
@@ -0,0 +1,24 @@
+package variables;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        // Numeric data types
+        // byte -128 --> 127
+        byte smallNumber = 127;
+
+        // short -32,768 --> 32,767
+        short shortNumber = 32767;
+
+        // int -2,147,483,648 --> 2,147,483,647
+        int normalNumber = 2147483647;
+
+        // long -9,223,372,036,854,775,808 --> 9,223,372,036,854,775,807
+        long longNumber = 9223372036854775807L;
+
+        // float 1.40129846432481707e-45 --> 3.4028234663858860e38
+        float normalDecimal = 5.154874558F;
+
+        // double 4.94065645841246544e-324d --> 1.79769313486231570e308d
+        double longDecimal = 5478.9862214588555;
+    }
+}
